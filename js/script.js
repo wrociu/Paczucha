ec751d281016fc1fc41a1e81ba51e3d58ab68506
piotr.wrotek <Piotr.Wrotek@canalplus.pl>
2020-05-14 15:20:59 +0200

    Czytanie XML z pliku

--- js/script.js
+++ js/script.js
@@ -2,21 +2,55 @@ var xml_text, parser, xmlDoc;
 var packages = [];
 var packageSteps = [];
 
+/* #region open File - handler */
+function readBlob() {
 
+  var files = document.getElementById('files').files;
+  if (!files.length) {
+    alert('Please select a file!');
+    return;
+  }
+
+  var file = files[0];
+  var start = 0;
+  var stop = file.size - 1;
+
+  var reader = new FileReader();
+
+  // If we use onloadend, we need to check the readyState.
+  reader.onloadend = function (evt) {
+    if (evt.target.readyState == FileReader.DONE) { // DONE == 2
+      document.getElementById('byte_content').textContent = evt.target.result;
+      xml_text = evt.target.result;
+      START();
+    }
+  };
+
+  var blob = file.slice(start, stop + 1);
+  reader.readAsBinaryString(blob);
+  
+}
+
+document.querySelector('.readBytesButtons').addEventListener('click', function (evt) {
+  if (evt.target.tagName.toLowerCase() == 'button') {
+    readBlob();
+  }
+}, false);
+/* #endregion */
 
 /* #region XML source mockup */
-xml_text = "<INSTRUKCJA>" +
-  "<PACZKA nazwa='BUG_1170_v4'>" +
-  "<STEP>1</STEP>" +
-  "<STEP_DESCRIPTION>Zainstaluje pakiety</STEP_DESCRIPTION>" +
-  "<STEP>2</STEP>" +
-  "<STEP_DESCRIPTION>Puśc skrypt konfiguracyjny</STEP_DESCRIPTION>" +
-  "</PACZKA>" +
-  "<PACZKA nazwa='CL_1787_Fix_v2'>" +
-  "<STEP>1</STEP>" +
-  "<STEP_DESCRIPTION>Puśc skrypt konfiguracyjny numer 2</STEP_DESCRIPTION>" +
-  "</PACZKA>" +
-  "</INSTRUKCJA>";
+// xml_text = "<INSTRUKCJA>" +
+//   "<PACZKA nazwa='BUG_1170_v4'>" +
+//   "<STEP>1</STEP>" +
+//   "<STEP_DESCRIPTION>Zainstaluje pakiety</STEP_DESCRIPTION>" +
+//   "<STEP>2</STEP>" +
+//   "<STEP_DESCRIPTION>Puśc skrypt konfiguracyjny</STEP_DESCRIPTION>" +
+//   "</PACZKA>" +
+//   "<PACZKA nazwa='CL_1787_Fix_v2'>" +
+//   "<STEP>1</STEP>" +
+//   "<STEP_DESCRIPTION>Puśc skrypt konfiguracyjny numer 2</STEP_DESCRIPTION>" +
+//   "</PACZKA>" +
+//   "</INSTRUKCJA>";
 /* #endregion */
 
 
@@ -42,39 +76,41 @@ function RedrawProgressBar(progress, all) {
 
 
 /* #region Loading data to packages and  packageSteps*/
-parser = new DOMParser();
-xmlDoc = parser.parseFromString(xml_text, "text/xml");
+function START() {
+  parser = new DOMParser();
+  xmlDoc = parser.parseFromString(xml_text, "text/xml");
 
-printTXT = '';
-txt = "";
-x = xmlDoc.getElementsByTagName("STEP");
-y = xmlDoc.getElementsByTagName("STEP_DESCRIPTION");
+  printTXT = '';
+  txt = "";
+  x = xmlDoc.getElementsByTagName("STEP");
+  y = xmlDoc.getElementsByTagName("STEP_DESCRIPTION");
 
-for (i = 0; i < x.length; i++) {
-  var p = x[i].parentNode.getAttribute('nazwa');
-  var s = x[i].childNodes[0].nodeValue;
-  var d = y[i].childNodes[0].nodeValue;
+  for (i = 0; i < x.length; i++) {
+    var p = x[i].parentNode.getAttribute('nazwa');
+    var s = x[i].childNodes[0].nodeValue;
+    var d = y[i].childNodes[0].nodeValue;
 
-  packages.push(x[i].parentNode.getAttribute('nazwa'));
-  var packageStep = {
-    package: p,
-    step: s,
-    description: d
-  };
-  packageSteps.push(packageStep);
+    packages.push(x[i].parentNode.getAttribute('nazwa'));
+    var packageStep = {
+      package: p,
+      step: s,
+      description: d
+    };
+    packageSteps.push(packageStep);
 
-  txt += packageStep.package;
-  txt += packageStep.step;
-  txt += packageStep.description;
+    txt += packageStep.package;
+    txt += packageStep.step;
+    txt += packageStep.description;
+  }
+
+  PrintPackagesSteps(packageSteps);
+  PrintPackagesList(packages);
 }
-
-PrintPackagesSteps(packageSteps);
-PrintPackagesList(packages);
 /* #endregion */
 
 /* #region Packages list */
 function PrintPackagesList(packagesArray) {
-  
+
   var packagesSet = new Set(packagesArray);
   packagesSet.forEach(JoinTxtForPackagesList);
 
@@ -85,20 +121,20 @@ function JoinTxtForPackagesList(element) {
   var input = document.createElement("input");
   input.type = "checkbox";
   input.id = element;
-  
+
   var label = document.createElement("label");
-  label.setAttribute("for",element);  
-  
+  label.setAttribute("for", element);
+
   var span = document.createElement("span");
-  
+
   var checkboxes = document.getElementById("checkboxes");
-  
-  var t = document.createTextNode(element);    
+
+  var t = document.createTextNode(element);
   label.appendChild(span);
   label.appendChild(t);
   checkboxes.appendChild(input);
   checkboxes.appendChild(label);
-  checkboxes.appendChild(document.createElement("br"));  
+  checkboxes.appendChild(document.createElement("br"));
 
 }
 /* #endregion */
@@ -111,24 +147,25 @@ function PrintPackagesSteps(packagesStepsArray) {
   }
 }
 
-function printCheckboxesToClick(packageName, step, description)
-{ 
-  var idk = packageName+step;  
-  
+function printCheckboxesToClick(packageName, step, description) {
+  var idk = packageName + step;
+
   var input = document.createElement("input");
   input.type = "checkbox";
   input.name = packageName;
   input.id = idk;
-  input.onclick = function(){ ValidatePackages(this, packageName); };
-  
+  input.onclick = function () {
+    ValidatePackages(this, packageName);
+  };
+
   var label = document.createElement("label");
-  label.setAttribute("for",idk);  
-  
+  label.setAttribute("for", idk);
+
   var span = document.createElement("span");
-  
+
   var checkboxesToClick = document.getElementById("checkboxesToClick");
-  
-  var t = document.createTextNode(step + "  |  " + packageName + "  |  " + description);    
+
+  var t = document.createTextNode(step + "  |  " + packageName + "  |  " + description);
   label.appendChild(span);
   label.appendChild(t);
   checkboxesToClick.appendChild(input);
@@ -137,39 +174,38 @@ function printCheckboxesToClick(packageName, step, description)
 }
 /* #endregion */
 
-function ValidatePackages(checkedEl, idToCheck)
-{   
+function ValidatePackages(checkedEl, idToCheck) {
 
   checkedByNameCount = CountCheckedCheckboxes(checkedEl.name);
   allCheckedByNameCount = CountCheckboxes(checkedEl.name);
 
-  if (checkedByNameCount == allCheckedByNameCount)
-    {document.getElementById(idToCheck).checked = true;}
-  else
-    {document.getElementById(idToCheck).checked = false;}
-  
+  if (checkedByNameCount == allCheckedByNameCount) {
+    document.getElementById(idToCheck).checked = true;
+  } else {
+    document.getElementById(idToCheck).checked = false;
+  }
+
   var checkedCheckboxes = CountCheckedCheckboxes();
   var allCheckboxes = CountCheckboxes();
-  
-  RedrawProgressBar(checkedCheckboxes, allCheckboxes);  
-  
+
+  RedrawProgressBar(checkedCheckboxes, allCheckboxes);
+
 }
 
 // parName can be null
 // if null it counts for all checkboxes
-function CountCheckedCheckboxes(parName)
-{
+function CountCheckedCheckboxes(parName) {
   var count = 0;
-  var checkboxes = document.getElementById("checkboxesToClick"); 
+  var checkboxes = document.getElementById("checkboxesToClick");
   var inputElems = checkboxes.getElementsByTagName("input");
-  
+
   if (name == null)
     console.log("name jest null");
 
-  for (var i=0; i<inputElems.length; i++) 
-  {       
-    if (inputElems[i].type == "checkbox" && inputElems[i].checked == true && (inputElems[i].name == parName || parName == undefined))
-      { count++; }      
+  for (var i = 0; i < inputElems.length; i++) {
+    if (inputElems[i].type == "checkbox" && inputElems[i].checked == true && (inputElems[i].name == parName || parName == undefined)) {
+      count++;
+    }
   }
 
   return count;
@@ -178,16 +214,15 @@ function CountCheckedCheckboxes(parName)
 
 // parName can be null
 // if null it counts for all checkboxes
-function CountCheckboxes(parName)
-{
+function CountCheckboxes(parName) {
   var count = 0;
-  var checkboxes = document.getElementById("checkboxesToClick"); 
-  var inputElems = checkboxes.getElementsByTagName("input");  
+  var checkboxes = document.getElementById("checkboxesToClick");
+  var inputElems = checkboxes.getElementsByTagName("input");
 
-  for (var i=0; i<inputElems.length; i++) 
-  {       
-    if (inputElems[i].type == "checkbox" && (inputElems[i].name == parName || parName == undefined))
-      { count++; }      
-  }  
+  for (var i = 0; i < inputElems.length; i++) {
+    if (inputElems[i].type == "checkbox" && (inputElems[i].name == parName || parName == undefined)) {
+      count++;
+    }
+  }
   return count;
 }
